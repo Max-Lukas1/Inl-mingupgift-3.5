eda346a6475b1f0fb14f81d5a677a1e29b7012a3
majo06002 <Max-Lukas@outlook.com>
2023-10-06 13:41:18 +0200

    Add project files.

---
+++ Inlämingupgift 3.5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace inlämningsuppgift3_5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välj ett heltal:");
+            int heltal1= int.Parse(Console.ReadLine());
+            Console.WriteLine("Välj ett till heltal:");
+            int heltal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Välj mellan 1. addition 2. subtraktion 3. multiplikation 4. division");
+            String x = Console.ReadLine();
+            switch (x) 
+            {
+
+                case "1":
+                    Console.WriteLine(heltal1 + heltal2);
+                    break;
+                case "2":
+                    Console.WriteLine(heltal1 - heltal2);
+                    break;
+
+                case "3":
+                    Console.WriteLine(heltal1 * heltal2);
+                    break;
+
+                case "4":
+                    Console.WriteLine(heltal1 / heltal2);
+                    break;
+
+
+
+            }
+
+        }
+    }
+}
+
